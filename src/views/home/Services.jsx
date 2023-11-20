7e3fb9784643b67d18f18f056f18fb93fe266cfc
Haseebinvader <rhaseeb741@gmail.com>
2023-11-20 14:11:37 +0500

    update

--- src/views/home/Services.jsx
+++ src/views/home/Services.jsx
@@ -1,26 +1,28 @@
 import { Box, Grid, Hidden, Typography } from "@mui/material"
 import { Images } from "../../Data/Home"
-import { Home_Data } from "../../Data/Project_Data"
+import { useTranslation } from 'react-i18next'
 const Services = () => {
+    const { t } = useTranslation();
+
     return (
         <Grid container sx={{ display: 'flex', justifyContent: 'space-around', mt: '2rem', height: '100%', pb: '2rem' }}>
             <Grid item>
-                <Typography textAlign='center' fontSize='35px'>{Home_Data.Propos_Title}</Typography>
+                <Typography textAlign='center' fontSize='35px'>{t('Propos_Title')}</Typography>
                 <Box style={{ width: '100% ', height: '2px', backgroundColor: 'red', margin: '8px 0', }} ></Box>
                 <Box sx={{ display: 'flex', justifyContent: 'space-evenly', mt: '4rem' }}>
                     <img src={Images.propos} width='50%' />
                     <Hidden mdDown>
-                        <Typography width='18rem' fontSize='14px' pl='1rem'>{Home_Data.Propos_Description}</Typography>
+                        <Typography width='18rem' fontSize='14px' pl='1rem'>{t('Propos_Description')}</Typography>
                     </Hidden>
                 </Box>
             </Grid>
             <Grid item>
-                <Typography textAlign='center' fontSize='35px'>{Home_Data.Services_Title}</Typography>
+                <Typography textAlign='center' fontSize='35px'>{t('Services_Title')}</Typography>
                 <Box style={{ width: '100% ', height: '2px', backgroundColor: 'red', margin: '8px 0', }} ></Box>
                 <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: '4rem' }}>
                     <img src={Images.services} width='50%' />
                     <Hidden mdDown>
-                        <Typography width='18rem' fontSize='14px' pl='1rem'>{Home_Data.Services_Description}</Typography>
+                        <Typography width='18rem' fontSize='14px' pl='1rem'>{t('Services_Description')}</Typography>
                     </Hidden>
                 </Box>
             </Grid>
